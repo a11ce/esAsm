fd25f5079e697b821589875ad356d2b653de1673
a11ce <a11ce@protonmail.com>
2020-12-10 02:35:05 -0500

    YAPF

--- cppTranspiler.py
+++ cppTranspiler.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def main():
     program, labels = loadProgram(sys.argv[1])
 
@@ -11,19 +12,19 @@ def main():
     print("{")
     for i in range(12):
         print("\t" + "int r" + str(i) + " = 0;")
-        
+
     print()
-        
+
     for curOp in program:
 
         for i in range(len(curOp)):
-            if(curOp[i][0] == "#"):
+            if (curOp[i][0] == "#"):
                 curOp[i] = curOp[i][1:]
-            if(curOp[i] in labels):
+            if (curOp[i] in labels):
                 curOp[i] = str(labels[curOp[i]])
 
         opCode = curOp[0]
-        
+
         print("\tl" + str(labelCount) + ":")
         #print(curOp)
 
@@ -43,31 +44,36 @@ def main():
             print("\t" + curOp[1] + " = " + curOp[2] + " - " + curOp[3] + ";")
 
         if opCode == "jlt":
-            print("\t" + "if (" + curOp[2] + " < " + curOp[3] +"){goto l" + curOp[1] +";}")
+            print("\t" + "if (" + curOp[2] + " < " + curOp[3] + "){goto l" +
+                  curOp[1] + ";}")
 
         if opCode == "jgt":
-            print("\t" + "if (" + curOp[2] + " > " + curOp[3] +"){goto l" + curOp[1] +";}")
+            print("\t" + "if (" + curOp[2] + " > " + curOp[3] + "){goto l" +
+                  curOp[1] + ";}")
 
         if opCode == "jet":
-            print("\t" + "if (" + curOp[2] + " == " + curOp[3] +"){goto l" + curOp[1] +";}")
+            print("\t" + "if (" + curOp[2] + " == " + curOp[3] + "){goto l" +
+                  curOp[1] + ";}")
 
         if opCode == "jmp":
             print("\t" + "goto l" + curOp[1] + ";")
 
         if opCode == "inp":
-            print("\t" + "cin >> " + curOp[1] +";")
+            print("\t" + "cin >> " + curOp[1] + ";")
 
         if opCode == "hlt":
             print("\t" + "exit(0);")
-            
+
         labelCount += 1
         print()
-        
+
     print("\t" + "goto l0;")
     print("}")
+
+
 def loadProgram(filename):
     program = []
-    labels  = {}
+    labels = {}
     with open(filename) as f:
         for line in f:
             noComLine = line.split(';')[0].strip()
@@ -79,6 +85,7 @@ def loadProgram(filename):
                     program.append(noComLine.split(" "))
     #print(program)
     return program, labels
-    
+
+
 if __name__ == "__main__":
-    main()    
+    main()
